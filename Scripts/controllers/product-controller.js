87cd95073d14a4c4b54569b6ad051c517b1b9ab7
Ayush502-cell <dobhalayush0@gmail.com>
2025-07-29 21:40:56 +0530

    added json file for data

--- Scripts/controllers/product-controller.js
+++ Scripts/controllers/product-controller.js
@@ -1,14 +1,42 @@
-// Glue between UI and Model
-function doNetworkCall(){
-    const URL = "https://gist.githubusercontent.com/kshirsagarps/36fade16fa39202715656ef487aaf7b0/raw/2b682e589ef283f06be42d2799dfa54f57794a6e/Pizza.json";
-    const promise = fetch(URL);
-    console.log("promise is : ",promise);
-    promise.then(response=>{
-        console.log("response is : ",response);
-        const promise2 = response.json();
-        promise2.then(data=>console.log("Data is",data))
-        .catch(e=>console.log("JSON parse error",e))
-    }).catch(err=>{
-        console.log("error is : ",err);
-    })
+
+
+import productOperations from "../Services/product-operations.js";
+async function loadPizzas(){
+   const pizzas = await productOperations.loadProducts();
+   console.log("Pizzas are",pizzas);
+   for(let pizza of pizzas){
+    preparePizzaCard(pizza);
+   }
+}
+loadPizzas();
+
+
+function preparePizzaCard(pizza){
+    const output = document.querySelector("#outputdiv");
+    const colDiv = document.createElement('div');
+    colDiv.className = 'col-4';
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+    cardDiv.style = "width: 18rem;";
+    colDiv.appendChild(cardDiv);
+    const img = document.createElement('img');
+    img.src = pizza.url;
+    img.className = 'card-img-top';
+    cardDiv.appendChild(img);
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    cardDiv.appendChild(cardBody);
+    const h5 = document.createElement('h5');
+    h5.className = 'card-title';
+    h5.innerText = pizza.name;
+    const pTag = document.createElement('p');
+    pTag.className = 'card-text';
+    pTag.innerText = pizza.desc;
+    const button = document.createElement('button');
+    button.innerText = 'Add to Cart';
+    button.className = 'btn btn-primary';
+    cardBody.appendChild(h5);
+    cardBody.appendChild(pTag);
+    cardBody.appendChild(button);
+    output.appendChild(colDiv);
 }
